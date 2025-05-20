379555d5febcdd6eaa82267551792fa3dcb4e55f
ChocolateKest <gustavoemartins@gmail.com>
2025-05-20 19:44:53 -0300

    .aa

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,16 @@
+package principal;
+
+import model.Mediana;
+
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args){
+        Mediana mediana = new Mediana();
+        mediana.setAluno(JOptionPane.showInputDialog("Digite o nome do aluno: "));
+        mediana.setNota1(Double.parseDouble(JOptionPane.showInputDialog("Digite a nota do primeiro Trimestre: ")));
+        mediana.setNota2(Double.parseDouble(JOptionPane.showInputDialog("Digite a nota do segundo Trimestre: ")));
+        mediana.setNota3(Double.parseDouble(JOptionPane.showInputDialog("Digite a nota do terceiro Trimestre: ")));
+        JOptionPane.showMessageDialog(null,mediana.toString());
+    }
+}
